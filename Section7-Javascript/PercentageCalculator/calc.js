19c5c5248b29d5313603ca4bad3b877b07e4a361
Alexis Huerta <alehuer90@gmail.com>
2019-07-02 12:17:42 -0700

    Finished Sec7.55

--- Section7-Javascript/PercentageCalculator/calc.js
+++ Section7-Javascript/PercentageCalculator/calc.js
@@ -1,11 +1,20 @@
 var  numField1 = document.getElementById('numField1');
 var  numField2 = document.getElementById('numField2');
-
 var resultField = document.getElementById('resultField');
 
-numField1.value = "Hey Bob!";
-numField2.value = "TEST";
+//adding event listener to form
+var form = document.getElementById('xisWhatPercentOfY')
+form.addEventListener('submit', function() {
 
-resultField.innerText = "Result";
+    //if this is null or undefined
+    if(!numField1.value || !numField2.value)
+    {
+        alert("Please enter values in the fields")
+    }
 
-alert(numField1.value);
+    //parsing the string into a float value
+    var x = parseFloat(numField1.value);
+    var y = parseFloat(numField2.value);
+
+    alert(x + y);
+});
