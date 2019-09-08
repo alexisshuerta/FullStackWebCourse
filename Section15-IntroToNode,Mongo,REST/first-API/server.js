a55c638c0f5b1a26f02372085a6c339c7f55a9ba
Alexis Huerta <alehuer90@gmail.com>
2019-09-07 17:19:32 -0700

    Finished Sec15.135

--- Section15-IntroToNode,Mongo,REST/first-API/server.js
+++ Section15-IntroToNode,Mongo,REST/first-API/server.js
@@ -1,14 +1,48 @@
 var express = require("express"); // Acquire a package that allows for incomming of requests and the sending pack of responses
-var app = express(); //--> var app = require('express')();
-//
+var app = express(); //All in one --> var app = require('express')();
+var bodyParser = require("body-parser");
+
+//should be here for (good for get and post requests)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//a list of ingredients as an array of objects [{_,_},{_,_},{_,_}]
+var ingredients = [
+  {
+    id: "232kak",
+    text: "Eggs"
+  },
+  {
+    id: "dkP345",
+    text: "Milk"
+  },
+  {
+    id: "dkcuu7",
+    text: "Bacon"
+  },
+  {
+    id: "73hdy",
+    text: "Frogs Legs"
+  }
+];
+
 app.get("/", function(request, response) {
   //when someone gets to base url, pass in a request and send back a response
-  response.send("My First API!"); // ALWAYS SEND SOMETHING BACK
+  response.send(ingredients); // ALWAYS SEND SOMETHING BACK
 });
 
-app.get("/funions", function(request, response) {
-  //when someone gets to /funions url, pass in a request and send back a response
-  response.send("YO give me some funions Foo!"); // ALWAYS SEND SOMETHING BACK
+app.post("/", function(request, response) {
+  var ingredient = request.body;
+  //if error
+  if (!ingredient || ingredient.text === "") {
+    response.status(500).send({ error: "Your ingredient must have text" });
+    //if successful
+  }
+  //if successful
+  else {
+    ingredients.push(ingredient);
+    response.status(200).send(ingredient);
+  }
 });
 
 app.listen(3000, function() {
