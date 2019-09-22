171512554d6e22a8a557c485792209128a526ec6
Alexis Huerta <alehuer90@gmail.com>
2019-09-21 22:42:45 -0700

    Finished Sec15

--- Section15-IntroToNode,Mongo,REST/swag-shop-api/server.js
+++ Section15-IntroToNode,Mongo,REST/swag-shop-api/server.js
@@ -10,7 +10,7 @@ var WishList = require("./model/wishlist");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//post request which allows client side to put in new items in DB
+//post request to put in new items in DB
 app.post("/product", function(req, res) {
   var product = new Product(); //mongoose model has constructor syntax
   product.title = req.body.title;
@@ -25,9 +25,10 @@ app.post("/product", function(req, res) {
   });
 });
 
-//get request which displays all elements in DB
+//get request for products which displays all elements in DB
 app.get("/product", function(req, res) {
   //.find() sends back an array, if you want a specific product, you need to access the certain element in the array
+  // {} means you want to find all; no specific attributes were included in the search
   Product.find({}, function(err, products) {
     if (err) {
       res.status(500).send({ error: "Could not fetch products" });
@@ -36,6 +37,67 @@ app.get("/product", function(req, res) {
     }
   });
 });
+
+//post request to put in new wishlist in DB
+app.post("/wishlist", function(req, res) {
+  var wishList = new WishList();
+  wishList.title = req.body.title;
+  wishList.save(function(err, newWishList) {
+    if (err) {
+      res.send({ error: "Could not create wishlist" });
+    } else {
+      res.send(newWishList);
+    }
+  });
+});
+
+//put request to add an item to the wishlist (put == update) updating wishlist
+app.put("/wishlist/product/add", function(req, res) {
+  //find one *** product in DB
+  Product.findOne({ _id: req.body.productId }, function(err, product) {
+    if (err) {
+      res.status(500).send({ error: "Could not add item to wishlist" });
+    }
+    //update(find product, set/add, usual callback)
+    else {
+      WishList.update(
+        { _id: req.body.wishListId },
+        { $addToSet: { products: product._id } },
+        function(err, wishList) {
+          if (err) {
+            res.status(500).send({ error: "Could not add item to wishlist" });
+          } else {
+            res.send("Successfully added to wishlist");
+          }
+        }
+      );
+    }
+  });
+});
+
+//get request to get the list of wishlists, now inclueds product data (using populate*)
+app.get("/wishlist", function(req, res) {
+  WishList.find({})
+    .populate({ path: "products", model: "Product" })
+    .exec(function(err, wishLists) {
+      if (err) {
+        res.status(500).send({ error: "Could not fetch wishlists" });
+      } else {
+        res.send(wishLists);
+      }
+    });
+});
+
+// //get request to get the list of wishlists
+// app.get("/wishlist", function(req, res) {
+//   WishList.find({}, function(err, wishLists) {
+//     if (err) {
+//       res.status(500).send({ error: "Could not fetch wishlists" });
+//     } else {
+//       res.send(wishLists);
+//     }
+//   });
+// });
 
 app.listen(3000, function() {
   console.log("Swag Shop API running on port 3000...");
