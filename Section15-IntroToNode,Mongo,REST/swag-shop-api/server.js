3d5b10fdb8f6cf3c5c40a901c07041be669b9d1b
Alexis Huerta <alehuer90@gmail.com>
2019-09-21 20:16:07 -0700

    Finished Sec15.144

--- Section15-IntroToNode,Mongo,REST/swag-shop-api/server.js
+++ Section15-IntroToNode,Mongo,REST/swag-shop-api/server.js
@@ -10,6 +10,7 @@ var WishList = require("./model/wishlist");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//post request which allows client side to put in new items in DB
 app.post("/product", function(req, res) {
   var product = new Product(); //mongoose model has constructor syntax
   product.title = req.body.title;
@@ -20,6 +21,18 @@ app.post("/product", function(req, res) {
       res.status(500).send({ error: "Could not save product" });
     } else {
       res.status(200).send(savedProduct); //common in rest api, to send the saved product
+    }
+  });
+});
+
+//get request which displays all elements in DB
+app.get("/product", function(req, res) {
+  //.find() sends back an array, if you want a specific product, you need to access the certain element in the array
+  Product.find({}, function(err, products) {
+    if (err) {
+      res.status(500).send({ error: "Could not fetch products" });
+    } else {
+      res.send(products);
     }
   });
 });
